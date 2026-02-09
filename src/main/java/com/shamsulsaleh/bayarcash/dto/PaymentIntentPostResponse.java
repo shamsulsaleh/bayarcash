6c47c35fe063cb59d780f0ea696e412ddb71b47c
Shamsul Saleh <Shamsul.saleh@gmail.com>
2026-02-09 19:51:31 +0800

    update checksum validation logic

--- src/main/java/com/shamsulsaleh/bayarcash/dto/PaymentIntentPostResponse.java
+++ src/main/java/com/shamsulsaleh/bayarcash/dto/PaymentIntentPostResponse.java
@@ -1,4 +1,7 @@
 package com.shamsulsaleh.bayarcash.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -6,10 +9,18 @@ public class PaymentIntentPostResponse {
 
     private String type; // only available on v3
     private String id; // only available on v3
+    @JsonProperty("payer_name")
     private String payerName;
+
+    @JsonProperty("payer_email")
     private String payerEmail;
+
+    @JsonProperty("payer_telephone_number")
     private String payerTelephoneNumber;
+
+    @JsonProperty("order_number")
     private String orderNumber;
+
     private String amount;
     private String url;
 }
